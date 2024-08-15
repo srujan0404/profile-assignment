36247649df0c90741b98b8792e6687017bed49b8
srujan0404 <srujan.23bcs10107@ms.sst.scaler.com>
2024-08-15 08:28:43 +0530

    completed the product pages

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import cart from '../assets/cart.png';
+import { useSelector, useDispatch } from 'react-redux';  
+import { addToCart } from '../store/cart';
 
 const Product = ({id, name, price, image, slug}) => {
+
+  const carts = useSelector((state) => state.cart.items);
+  console.log(carts);
+
+  const dispatch = useDispatch();
+  const handleAddToCart = () => {
+    dispatch(addToCart({ productId: id, quantity: 1 }));
+  }
 
   return (
     <div className="bg-white p-5 rounded-lg shadow-md mt-5">
@@ -9,21 +20,21 @@ const Product = ({id, name, price, image, slug}) => {
         <img
           src={image}
           alt="product-image"
-          className="w-full h-80 object-cover object-top drop-shadow-[0_80px_30px_#0007]"
+          className="w-full h-80 object-cover object-top drop-shadow-lg"
         />
       </Link>
       <h3 className="text-lg py-3 text-center font-medium">{name}</h3>
       <div className="flex justify-between items-center">
         <p>
-          ₹<span className="text-2xl font-medium">{price}</span>
+          <strong>₹</strong><span className="text-2xl font-semibold">{price}</span>
         </p>
-        {/* <button
+        <button
           className="bg-gray-300 p-2 rounded-md text-sm hover:bg-gray-400 flex gap-2"
           onClick={handleAddToCart}
         >
-          <img src={iconCart} alt="" className="w-5" />
+          <img src={cart} alt="" className="w-5" />
           Add To Cart
-        </button> */}
+        </button>
       </div>
     </div>
   );
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import products  from '../products';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../store/cart';
 
 const Details = () => {
+  const { slug } = useParams();
+  const [details, setDetails] = useState([]);
+  const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const product = products.filter(product => product.slug === slug);
+    if(product.length > 0) {
+      setDetails(product[0]);
+    }else{
+        window.location.href = '/';
+    }
+  }, [slug])
+
+  const handleMinusQuantity = () => {
+    setQuantity(quantity - 1 < 1 ? 1 : quantity - 1);
+  }
+
+  const handlePlusQuantity = () => {
+    setQuantity(quantity + 1);
+  }
+
+  const handleAddToCart = () => {
+   dispatch(addToCart({
+    productId: details.id,
+    quantity: quantity,
+   })); 
+  }
+  
   return (
     <div>
-      Details
+      <h2 className="text-3xl text-center">PRODUCT DETAIL</h2>
+      <div className="grid grid-cols-2 gap-5 mt-5">
+        <div>
+          <img src={details.image} alt="" className="w-full" />
+        </div>
+        <div className="flex flex-col gap-5">
+          <h1 className="text-4xl uppercase font-bold">{details.name}</h1>
+          <p className="font-bold text-3xl">₹{details.price}</p>
+          <div className="flex gap-5">
+            <div className="flex gap-2 justify-center items-center">
+              <button
+                className="bg-gray-100 h-full w-10 font-bold text-xl rounded-xl flex justify-center items-center"
+                onClick={handleMinusQuantity}
+              >
+                -
+              </button>
+              <span className="bg-gray-200 h-full w-10 font-bold text-xl rounded-xl flex justify-center items-center">
+                {quantity} 
+              </span>
+              <button
+                className="bg-gray-100 h-full w-10 font-bold text-xl rounded-xl flex justify-center items-center"
+                onClick={handlePlusQuantity}
+              >
+                +
+              </button>
+            </div>
+            <button
+              className="bg-slate-900 text-white px-7 py-3 rounded-xl shadow-2xl"
+              onClick={handleAddToCart}
+            >
+              Add To Cart
+            </button>
+          </div>
+          <p>{details.description}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Details
